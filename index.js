eae21c3aecc4b835779fddcc7bb92dbadc785e1d
BI-TENG <61406297+BI-TENG@users.noreply.github.com>
2020-04-28 15:39:12 +0800

    期中作業一開始

---
+++ index.js
@@ -0,0 +1,37 @@
+// 引用 linebot 套件
+import linebot from 'linebot'
+// 引用 dotenv 套件
+import dotenv from 'dotenv'
+// 引用 request 套件，request 在後端用，不再前端用
+import rp from 'request-promise'
+
+// 讀取 .env 檔
+dotenv.config()
+
+const bot = linebot({
+  channelID: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+// 當收到訊息時
+// bot.on('message', event => {
+//   if (event.message.type === 'text') {
+//     event.reply(event.message.text)
+//   }
+// })
+bot.on('message', async (event) => {
+  let msg = ''
+  try {
+    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
+    msg = data.entry[0].title
+  } catch (error) {
+    msg = '發生錯誤'
+  }
+  event.reply(msg)
+})
+
+// 在 PORT 啟動
+bot.listen('/', process.env.PORT, () => {
+  console.log('機器人已啟動')
+})
