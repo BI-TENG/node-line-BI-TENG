10bd4aa76fa849cf2bfe6f2cede2aabfe7f0adf4
BI-TENG <SHIEBT.1026@gmail.com>
2020-05-04 14:09:40 +0800

    改成kkbox安裝黨

--- index.js
+++ index.js
@@ -2,36 +2,64 @@
 import linebot from 'linebot'
 // 引用 dotenv 套件
 import dotenv from 'dotenv'
-// 引用 request 套件，request 在後端用，不再前端用
-import rp from 'request-promise'
 
-// 讀取 .env 檔
+import kkbox from '@kkbox/kkbox-js-sdk'
+
+const Api = kkbox.Api
+const Auth = kkbox.Auth
+
+// 讀取 env 檔
 dotenv.config()
 
 const bot = linebot({
-  channelID: process.env.CHANNEL_ID,
+  channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-// 當收到訊息時
-// bot.on('message', event => {
+let kkboxToken = ''
+
+const getToken = async () => {
+  const auth = new Auth(process.env.KKBOX_ID, process.env.KKBOX_SECRET)
+  await auth.clientCredentialsFlow
+    .fetchAccessToken()
+    .then(response => {
+      kkboxToken = response.data.access_token
+    })
+}
+
+getToken()
+
+// // 當收到訊息時
+// bot.on('message', function (event) {
 //   if (event.message.type === 'text') {
-//     event.reply(event.message.text)
+//     event.source.profile().then(function (profile) {
+//       event.reply(profile.displayName + '你再說一次試試看')
+//     })
 //   }
 // })
+
 bot.on('message', async (event) => {
   let msg = ''
   try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
+    // Create an API object with your access token
+    const api = new Api(kkboxToken)
+
+    // Fetch content with various fetchers
+    await api.searchFetcher
+      .setSearchCriteria('五月天 派對動物', 'track')
+      .fetchSearchResult()
+      .then(response => {
+        // Content from the KKBOX Open API
+        console.log(response.data)
+      })
   } catch (error) {
     msg = '發生錯誤'
   }
   event.reply(msg)
 })
 
-// 在 PORT 啟動
+// 在 port 啟動
 bot.listen('/', process.env.PORT, () => {
   console.log('機器人已啟動')
 })
